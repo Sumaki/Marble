a2df54fac1b8a2deb43e28177f07d6670df5a8a2
Phi Dai Nguyen <iamphidainguyen@gmail.com>
2019-02-04 21:13:24 -0500

    Added project

---
+++ Marble/Assets/_MARBLE/_PHIDAI/Scripts/CameraFollow.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float cameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObj;
+    private Vector3 followPos;
+
+    public float clampAngle = 80.0f;
+    public float inputSens = 150f;
+    public GameObject cameraObj;
+    public GameObject playerObj;
+
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+
+    public float mouseX;
+    public float mouseY;
+
+    public float finalInputX;
+    public float finalInputZ;
+
+    public float smoothX;
+    public float smoothY;
+
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // setting up rotation of sticks
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSens * Time.deltaTime;
+        rotX += finalInputZ * inputSens * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        // set target object to follow
+        Transform target = cameraFollowObj.transform;
+
+        // move towards game object which is the target
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ Marble/Assets/_MARBLE/_PHIDAI/Scripts/Character_Ball.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Character_Ball : MonoBehaviour
+{
+    #region Character Varibles
+    Rigidbody rb;
+    public float xForce = 10.0f;
+    public float zForce = 10.0f;
+    public float yForce = 500.0f; // jump
+    public Transform cameraBase;
+
+    private Vector3 gravity = new Vector3(0f, -1000f, 0f);
+    #endregion
+
+    #region Movement Direction
+    private float horizontalMovement;
+    private float verticalMovement;
+    private Vector3 force = Vector3.zero;
+
+    Vector3 finalDirection = Vector3.zero;
+    #endregion
+
+    #region Test Display Varibles
+    public Text Horizontal;
+    public Text Vertical;
+    public Text Velocity;
+    public Text Position;
+    public Text FinalDirection;
+    #endregion
+
+    #region Public Testing Variables
+    public float gravityAmount;
+    #endregion
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gravity.y = -gravityAmount;
+        
+    }
+
+    private void Update()
+    {
+        DebugVariables();
+    }
+
+    private void FixedUpdate()
+    {       
+        BallInputs();
+        ApplyGravity();
+    }
+
+
+    /// <summary>
+    /// User inputs -> adjusting force and direction within camera -> move
+    /// </summary>
+    void BallInputs()
+    {
+
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        if (horizontalMovement != 0 || verticalMovement != 0)
+        {
+            float dt = Time.deltaTime;
+            Vector3 pNew = new Vector3(transform.position.x + horizontalMovement, transform.position.y, transform.position.z + verticalMovement); //our new desired position, check
+                                                                                                                                                  //Debug.Log(pNew);
+            Vector3 p = transform.position; //our current position
+            Vector3 v = rb.velocity; //our current velocity
+            force = rb.mass * (pNew - p - v * dt) / (dt); // check          
+
+            //return force;
+            //rb.AddForce(force);
+
+
+            Vector3 finalMovement = RotateInput();
+            if (rb.velocity.magnitude <= 30f)
+                rb.AddForce(finalMovement);
+        }
+        
+    }
+
+    Vector3 RotateInput()
+    {
+        Vector3 check = new Vector3(horizontalMovement, 0, verticalMovement);
+        finalDirection = cameraBase.transform.TransformDirection(check); // check
+        finalDirection.Set(finalDirection.x, 0, finalDirection.z);        
+        Vector3 move = finalDirection.normalized * force.magnitude;        
+        //Debug.Log(finalDirection.normalized * force.magnitude);
+        return move;
+    }
+
+    void ApplyGravity()
+    {
+        rb.AddForce(gravity);
+    }
+
+    void DebugVariables()
+    {
+        Horizontal.text = "Horizontal: " + horizontalMovement.ToString();
+        Vertical.text = "Vertical: " + verticalMovement.ToString();
+        Velocity.text = "Velocity: " + rb.velocity.ToString();
+        Position.text = "Position: " + transform.position.ToString();
+        FinalDirection.text = "FINAL DIRECTION: " + finalDirection.ToString();
+    }
+
+}
