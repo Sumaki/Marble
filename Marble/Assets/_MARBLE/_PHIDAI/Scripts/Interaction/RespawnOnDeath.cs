d489762cfd34b0618be9833807c9c28dc19d745c
Phi Dai Nguyen <iamphidainguyen@gmail.com>
2019-03-27 18:49:00 -0400

    Updated RespawnDeath

--- Marble/Assets/_MARBLE/_PHIDAI/Scripts/Interaction/RespawnOnDeath.cs
+++ Marble/Assets/_MARBLE/_PHIDAI/Scripts/Interaction/RespawnOnDeath.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RespawnOnDeath : MonoBehaviour
 {
     public GameObject player;
-    public GameManager playerHumanoid;
+   
+    public GameObject playerHumanoid;
     GameObject gm_;
 
     private void Start()
@@ -27,7 +28,7 @@ public class RespawnOnDeath : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
 
-        if (collision.gameObject.tag == "PlayerParent" || collision.gameObject.tag == "Player")
+        if (collision.gameObject == player || collision.gameObject == playerHumanoid)
         {
             Debug.Log("I died");
             player.transform.position = gm_.GetComponent<GameManager>().currentRespawn.position;
