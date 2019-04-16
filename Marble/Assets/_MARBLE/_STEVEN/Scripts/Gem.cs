b118b510fc54a06293950e46333c7e3bdd5190b8
Steven KS <stevks12@gmail.com>
2019-04-15 20:39:30 -0400

    new build

--- Marble/Assets/_MARBLE/_STEVEN/Scripts/Gem.cs
+++ Marble/Assets/_MARBLE/_STEVEN/Scripts/Gem.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Gem : MonoBehaviour
 {
     public Animator ani;
+    public Material newElementMaterial;
+    public GameObject humanoid_mesh;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "PlayerBall" || other.gameObject.tag == "PlayerHumanoid")
         {
+            
+          
+            StartCoroutine(TimerToSwtich());
             ani.SetTrigger("Open");
             Destroy(this.gameObject, 0.5f);
         }
+    }
+
+    IEnumerator TimerToSwtich()
+    {
+        yield return new WaitForSeconds(0.4f);
+        humanoid_mesh.GetComponent<SkinnedMeshRenderer>().material = newElementMaterial;
+
     }
 }
