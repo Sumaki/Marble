ea0355372a9bf7976bf3fe6e49e320c2fb042108
Steven KS <stevks12@gmail.com>
2019-05-08 12:06:00 -0400

    Gems Animation fixed

--- Marble/Assets/_MARBLE/_STEVEN/Scripts/Gem.cs
+++ Marble/Assets/_MARBLE/_STEVEN/Scripts/Gem.cs
@@ -8,6 +8,14 @@ public class Gem : MonoBehaviour
     public Material newElementMaterial;
     public GameObject humanoid_mesh;
 
+    // Floating Gem
+    public float degreePerSecond;
+    public float amplitude;
+    public float frequency;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "PlayerBall" || other.gameObject.tag == "PlayerHumanoid")
@@ -25,5 +33,20 @@ public class Gem : MonoBehaviour
         yield return new WaitForSeconds(0.4f);
         humanoid_mesh.GetComponent<SkinnedMeshRenderer>().material = newElementMaterial;
 
+    }
+
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreePerSecond, 0f), Space.World);
+
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
     }
 }
