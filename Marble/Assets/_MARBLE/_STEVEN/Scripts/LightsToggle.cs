6345e383f487dc35b16fd3b3746f1d5e1b49336a
Steven KS <stevks12@gmail.com>
2019-05-11 15:20:12 -0400

    Script for turning on lights at the start room

---
+++ Marble/Assets/_MARBLE/_STEVEN/Scripts/LightsToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsToggle : MonoBehaviour
+{
+    private GameObject[] Light;
+    public float WaitTime = 1f;
+
+    void Awake()
+    {
+        Light = GameObject.FindGameObjectsWithTag("Startlights");
+
+
+        foreach (GameObject i in Light)
+        {
+            i.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(TurnOn());
+    }
+
+    IEnumerator TurnOn()
+    {
+        yield return new WaitForSeconds(WaitTime);
+
+        foreach (GameObject i in Light)
+        {
+            i.SetActive(true);
+        }
+    }
+
+}
