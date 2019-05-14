679ad04bcf9348ef05f1c39f86b68a152528d724
Phi Dai Nguyen <iamphidainguyen@gmail.com>
2019-05-14 19:34:08 -0400

    Draft for pushing block in place puzzle
    + Code is there, not implemented yet onto the scene but will be soon
    + Two pieces of code: PuzzleConditions lets you set up what is needed to complete the puzzle and what object will be affected. BlockInPlace will allow the user to push the block into a small trigger that will make the object stuck and not be able to push.

---
+++ Marble/Assets/_MARBLE/_PHIDAI/Scripts/Interaction/Puzzle/BlockInPlace.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInPlace : MonoBehaviour
+{
+
+    public GameObject puzzleObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Pushable")
+        {
+            other.gameObject.transform.position = gameObject.transform.position;
+            // set that the box cannot be pushed anymore or perhaps change the color
+            puzzleObj.GetComponent<PuzzleConditions>().amountOfCompletedPieces += 1;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
---
+++ Marble/Assets/_MARBLE/_PHIDAI/Scripts/Interaction/Puzzle/PuzzleConditions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleConditions : MonoBehaviour
+{
+    public int amountOfCompletedPieces;
+    public int requiredAmountToComplete;
+    public GameObject objEvent;
+    public bool complete = false;
+    bool finishedAnimation = false;
+    Animator ani;
+    SkinnedMeshRenderer smr_objEvent;
+    float t = 0;
+
+    private void Start()
+    {
+        smr_objEvent = objEvent.GetComponent<SkinnedMeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if (amountOfCompletedPieces >= requiredAmountToComplete) complete = true;
+
+        if (complete && !finishedAnimation) DoCompletedAction();
+    }
+
+    void DoCompletedAction()
+    {
+        smr_objEvent.SetBlendShapeWeight(0, Mathf.Lerp(100, 0, t));
+        t += 3f * Time.deltaTime;
+
+        if (smr_objEvent.GetBlendShapeWeight(0) == 0) finishedAnimation = true;
+    }
+
+}
