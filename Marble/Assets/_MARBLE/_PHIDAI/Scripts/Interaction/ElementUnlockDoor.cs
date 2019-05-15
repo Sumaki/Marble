8b8053ab636879861328e0337e1b00deecef715c
Phi Dai Nguyen <iamphidainguyen@gmail.com>
2019-05-15 17:01:07 -0400

    Element door unlock
    + Script needs a trigger box to work
    + Set a element for that door and if the player has the element it will open

--- Marble/Assets/_MARBLE/_PHIDAI/Scripts/Interaction/ElementUnlockDoor.cs
+++ Marble/Assets/_MARBLE/_PHIDAI/Scripts/Interaction/ElementUnlockDoor.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class ElementUnlockDoor : MonoBehaviour
 {
-    
+    [Header("Which door's animator?")]
+    public Animator ani; 
+    public enum DoorType { Ruby, Sapphire, Amethyste }
+    [Header("What element does this door need?")]
+    public DoorType whichElement;
+    bool open = false;
+    GameObject gm_;
+
+    private void Start()
+    {
+        gm_ = GameObject.FindGameObjectWithTag("GameManager");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PlayerHumanoid" || other.gameObject.tag == "PlayerBall")
+        {
+            CheckWhichElement();
+            if (open)
+            {
+                ani.SetTrigger("Open");
+            }
+        }
+    }
+
+    void CheckWhichElement()
+    {
+        switch (whichElement)
+        {
+            case DoorType.Ruby:
+                if(gm_.GetComponent<GameManager>().gotRuby)
+                    open = true;
+                break;
+            case DoorType.Sapphire:
+                if (gm_.GetComponent<GameManager>().gotSapphire)
+                    open = true;
+                break;
+            case DoorType.Amethyste:
+                if (gm_.GetComponent<GameManager>().gotAmethyste)
+                    open = true;
+                break;
+        }
+    }
 }
