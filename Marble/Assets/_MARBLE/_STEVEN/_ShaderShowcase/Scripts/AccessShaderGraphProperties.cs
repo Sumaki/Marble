9f3faad6ab3c4e864f524911f76069e6d34ee045
Steven KS <stevks12@gmail.com>
2019-05-16 16:38:07 -0400

    ShaderShowcase from Tri added
    Example on how to access properties from Shader to a script

---
+++ Marble/Assets/_MARBLE/_STEVEN/_ShaderShowcase/Scripts/AccessShaderGraphProperties.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessShaderGraphProperties : MonoBehaviour
+{
+    [SerializeField] private float materialRadius = 1f;
+
+    private MeshRenderer meshRenderer = null;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        // the string used here is the one defined in the shader graph
+        meshRenderer.material.SetFloat("_Radius", materialRadius);
+    }
+}
